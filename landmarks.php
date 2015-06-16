83f3b78ce34fb47cd0e9d7d1a7f904810544233f
regdurose <reg@beckerhelicopters.com>
2015-06-16 16:34:40 +1000

    df

--- landmarks.php
+++ landmarks.php
@@ -9,7 +9,7 @@ $database = "landmarks";
 $con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
 mysql_select_db($database, $con);
 
-$sql = "SELECT id, l_name AS name, l_lat AS latitude, l_long AS longitude FROM landmarks ORDER BY l_name";
+$sql = "SELECT id, l_name AS name, l_lat AS latitude, l_long AS longitude FROM landmark ORDER BY l_name";
 $result = mysql_query($sql) or die ("Query error: " . mysql_error());
 
 $records = array();
